c89c3d4672f8f3cfef31efdf3b7ac0f17cf13bb4
Maickell Vilela <maickell.vilela@before.com.br>
2017-11-22 22:40:40 -0300

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,72 @@
 <template>
-  <div id="app">
-    <input type="text" @change="addItem($event.target.value)">
-    <ul>
-      <li v-for="item in list">{{item}}</li>
-    </ul>
-  </div>
+    <div id="app">
+        <input type="text"
+               :value="inputValue"
+               :placeholder="placeholer"
+               @change="addItem($event.target.value)">
+        <ul>
+            <li v-for="item in list">{{item}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      list: [
-          'leite',
-          'banana',
-          'chocolate',
-          'maça',
-          'pipoca',
-          'bolo'
-      ]
-    }
-  },
-    methods: {
-        addItem(value) {
-            this.list.push(value)
+    export default {
+        name: 'app',
+        data () {
+            return {
+                list: [
+                    'leite',
+                    'banana',
+                    'chocolate',
+                    'maça',
+                    'pipoca',
+                    'bolo'
+                ],
+                inputValue: '',
+                placeholer: 'shopping list'
+            }
+        },
+        methods: {
+            addItem(value) {
+                this.list.push(value)
+            }
         }
     }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-    border: 1px solid #bdbdbd;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+        border: 1px solid #bdbdbd;
+    }
 
-li {
-  display: block;
-  margin: 0 10px;
-}
+    li {
+        display: block;
+        margin: 0 10px;
+    }
 
-a {
-  color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
+
+    input {
+        padding:.3rem;
+        border-radius: .3rem;
+        border: 1px solid #bdbdbd;
+    }
 </style>
